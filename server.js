7abd627372f12399266a8ebdd4f941dccb25b1ba
Cavalet Pierre <pierre.cavalet@etu.umontpellier.fr>
2015-10-25 20:52:10 +0100

    envoie des BEEs en JSON

--- server.js
+++ server.js
@@ -4,13 +4,22 @@ var port = process.env.port || 1337;
 var server = app.listen(port);
 var io = require('socket.io')(server);
 
-setInterval(function () {
-    io.sockets.emit('message', "test");
-}, 3000);
 
 io.sockets.on('connection', function (socket) {
     console.log('Un client est connecte !');
-    socket.on('test', function (data) {
-        console.log("test: " + data);
+    socket.on('askBeesList', function (data) {
+        console.log("send bees");
+        var beesList = [];
+        for(var i=0; i<10; i++) {
+          beesList.push({
+            user: "Pierre",
+            location: "Perpignan",
+            time: "10H23",
+            content: "Mon rêve le plus fou, c'est chez afflelou!",
+            up: 10,
+            down: 20
+          });
+        }
+        socket.emit('beesList', beesList);
     });
 });
